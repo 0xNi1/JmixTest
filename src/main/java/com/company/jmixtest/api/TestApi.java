1229acfebf928e4e88082e37f1e671e5d7e605f4
Chaosglint <kevin1021studio@gmail.com>
2021-12-22 23:05:44 +0800

    KeyValueEntity 功能優化

--- src/main/java/com/company/jmixtest/api/TestApi.java
+++ src/main/java/com/company/jmixtest/api/TestApi.java
@@ -1,5 +1,6 @@
 package com.company.jmixtest.api;
 
+import com.company.jmixtest.helper.EzEntity;
 import com.company.jmixtest.service.TestOneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,15 +8,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
 public class TestApi {
 
     @Autowired
     private TestOneService t;
+
     @GetMapping("/")
-    public void test(){
-        t.testEntity();
+    public void test() {
+        EzEntity test = new EzEntity();
+        EzEntity test2 = new EzEntity();
+        List<EzEntity> test3 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            EzEntity tmp = new EzEntity();
+            tmp.addValue("Go" + i, i);
+            test3.add(tmp);
+        }
+        test2.setValue("test2", "gogo");
+        test.setValue("test3",test3);
+        test.setValue("test", test2);
+        test.setValue("goog", 1231);
+
+        System.out.println(test.toJsonString());
+        System.out.println(test.toJsonString());
+//        System.out.println(n.toJsonString());
+//        System.out.println(n.toJsonString());
+//        t.testEntity();
     }
 
 }
---
+++ src/main/java/com/company/jmixtest/helper/EzEntity.java
@@ -0,0 +1,81 @@
+package com.company.jmixtest.helper;
+
+import com.google.gson.Gson;
+import io.jmix.core.entity.KeyValueEntity;
+
+import java.util.*;
+
+public class EzEntity extends KeyValueEntity {
+
+
+    /**
+     * properties回傳
+     *
+     * @return Map
+     */
+    public Map<String, Object> getProperties() {
+        return super.properties;
+    }
+
+    /**
+     * 得到map所有Key
+     *
+     * @return
+     */
+    public Set<String> getNames() {
+        return super.properties.keySet();
+    }
+
+    /**
+     * 新增Vaule
+     *
+     * @param name
+     * @param v
+     * @return
+     */
+    public EzEntity addValue(String name, Object v) {
+        super.setValue(name, v);
+        return this;
+    }
+
+    /**
+     * Map轉Json
+     *
+     * @return
+     */
+    public String toJsonString() {
+        Map<String, Object> tmp = ezToMap();
+        return new Gson().toJson(tmp, LinkedHashMap.class);
+    }
+
+    /**
+     * EzEntity轉Map
+     *
+     * @return
+     */
+    public Map<String, Object> ezToMap() {
+        System.out.print("FUCK=>");
+        System.out.println(this.properties);
+        Map<String, Object> tmp = new HashMap<>(this.properties);
+        for (String v : tmp.keySet()) {
+            Object target = tmp.get(v);
+            if (target instanceof EzEntity) {
+                EzEntity tmp2 = (EzEntity) tmp.get(v);
+                tmp.put(v, tmp2.ezToMap());
+            } else if (target instanceof List) {
+                System.out.println(v+"===>"+target);
+                List<Map<String, Object>> addList = new ArrayList<>();
+                List<Object> tmpArray = (List<Object>) tmp.get(v);
+                for (Object x : tmpArray) {
+                    if (x instanceof EzEntity) {
+                        EzEntity tmp3 = (EzEntity) x;
+                        addList.add(tmp3.ezToMap());
+                    }
+                }
+                tmp.put(v, addList);
+            }
+        }
+        System.out.println(this.properties);
+        return tmp;
+    }
+}
